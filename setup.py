41937f4e257a227f9d6ad4d95814d9b459fbcbe4
pd-nate <nate@paralleldomain.com>
2022-11-17 23:30:51 -0800

    adjusted setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="ssdc",
     version="0.1.0",
-    packages=find_packages("src"),
+    packages=find_packages(),
     install_requires=[
         "numpy",
         "matplotlib",
